4ccb4325720e6e5054397e77d3fe863ce6df2d6c
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-16 17:14:08 -0600

    Mobile Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/shared/Layout/Layout";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
