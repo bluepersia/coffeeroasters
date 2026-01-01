13a0ce41f81d0fd4ecc1a69812f8f9118c46d569
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-01 14:47:40 -0600

    Our Collection styles

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero/Hero";
+import OurCollection from "./OurCollection/OurCollection";
 
 export default function Home() {
   return (
     <main aria-label="Home Main Content">
       <Hero />
+      <OurCollection />
     </main>
   );
 }
--- src/components/home/OurCollection/OurCollection.tsx
+++ src/components/home/OurCollection/OurCollection.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import clsx from "clsx";
 import { getOurCollection } from "./services";
 import CollectionItem from "./CollectionItem";
+import styles from "./our-collection.module.css";
 
 export default function OurCollection() {
   const { data } = useQuery({
@@ -9,11 +11,13 @@ export default function OurCollection() {
   });
 
   return (
-    <section>
-      <h2>Our Collection</h2>
-      {data?.map((item) => (
-        <CollectionItem {...item} />
-      ))}
+    <section className={styles.collection}>
+      <h2 className={clsx(styles.title, "text-9")}>Our Collection</h2>
+      <div className={styles.inner}>
+        {data?.map((item) => (
+          <CollectionItem {...item} />
+        ))}
+      </div>
     </section>
   );
 }
