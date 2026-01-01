d0d298edf2e3ecb23e0f9a17d14d1be66b1ad253
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-31 18:33:18 -0600

    Header TSX

---
+++ src/components/shared/Header/Header.tsx
@@ -0,0 +1,19 @@
+import MainNav from "../Nav/MainNav";
+import SiteLogo from "../SiteLogo/SiteLogo";
+import openNavMenuIcon from "../../../assets/shared/openNavMenu.png";
+import closeNavMenuIcon from "../../../assets/shared/closeNavMenu.png";
+
+export default function Header() {
+  const isOpen = false;
+  return (
+    <header>
+      <SiteLogo />
+      <MainNav />
+      <button
+        aria-label={isOpen ? "Close navigation menu" : "Open navigation menu"}
+      >
+        <img src={isOpen ? closeNavMenuIcon : openNavMenuIcon} />
+      </button>
+    </header>
+  );
+}
