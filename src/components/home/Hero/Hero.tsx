f66df71b0425d672bd1a0e4df4cd1589bc9bc6be
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-31 22:55:51 -0600

    Button preset

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
 import mainImg from "../../../assets/home/Hero.jpg";
+import styles from "./hero.module.css";
+import clsx from "clsx";
 
 export default function Hero() {
   return (
-    <section>
-      <h1>Great coffee made simple.</h1>
-      <p>
+    <section className={styles.hero}>
+      <h1 className={clsx(styles.title, "text-1")}>
+        Great coffee made simple.
+      </h1>
+      <p className={clsx(styles.desc, "text-6")}>
         Start your mornings with the world’s best coffees. Try our expertly
         curated artisan coffees from our best roasters delivered directly to
         your door, at your schedule.
       </p>
-      <Link to="/create-plan">Create your plan</Link>
-      <img src={mainImg} alt="Shimmering golden coffee pot and beans" />
+      <Link to="/create-plan" className={clsx(styles.link, "btn")}>
+        Create your plan
+      </Link>
+      <img
+        src={mainImg}
+        alt="Shimmering golden coffee pot and beans"
+        className={styles.img}
+      />
     </section>
   );
 }
