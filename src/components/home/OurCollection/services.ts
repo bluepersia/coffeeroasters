6a2611d7e7f9084a5903f468cdaf8b3b55b98dc7
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-01 12:11:54 -0600

    Dummy collection service + data

---
+++ src/components/home/OurCollection/services.ts
@@ -0,0 +1,22 @@
+const collection = [
+  {
+    name: "Gran Espresso",
+    desc: "Light and flavorful blend with cocoa and black pepper for an intense experience.",
+  },
+  {
+    name: "Planalto",
+    desc: "Brazilian dark roast with rich and velvety body, and hints of fruits and nuts.",
+  },
+  {
+    name: "Piccollo",
+    desc: "Mild and smooth blend featuring notes of toasted almond and dried cherry.",
+  },
+  {
+    name: "Danche",
+    desc: "Ethiopian hand-harvested blend densely packed with vibrant fruit notes.",
+  },
+];
+
+export default function getOurCollection() {
+  return collection;
+}
