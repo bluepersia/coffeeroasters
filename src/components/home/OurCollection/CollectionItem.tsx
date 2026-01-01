3f16ad89043cc9350b19d55b1ebd42bcef8f238a
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-01 14:52:19 -0600

    Our Collection mobile styles

--- src/components/home/OurCollection/CollectionItem.tsx
+++ src/components/home/OurCollection/CollectionItem.tsx
@@ -1,11 +1,17 @@
 import type { Props } from "./types";
+import styles from "./collection-item.module.css";
+import clsx from "clsx";
 
 export default function CollectionItem(props: Props) {
   return (
-    <article>
-      <img src={props.img} alt={`Bag of ${props.name}`} />
-      <h3>{props.name}</h3>
-      <p>{props.desc}</p>
+    <article className={styles.item}>
+      <img
+        src={props.img}
+        alt={`Bag of ${props.name}`}
+        className={styles.img}
+      />
+      <h3 className={clsx(styles.name, "text-4")}>{props.name}</h3>
+      <p className={clsx(styles.desc, "text-6")}>{props.desc}</p>
     </article>
   );
 }
