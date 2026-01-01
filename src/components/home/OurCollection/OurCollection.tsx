0ac4765ef87c9fa994081471f3df8466a21bc580
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-01 12:22:56 -0600

    OurCollection service images

---
+++ src/components/home/OurCollection/OurCollection.tsx
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import getOurCollection from "./services";
+
+export default function OurCollection() {
+  const { data } = useQuery({
+    queryKey: ["collection"],
+    queryFn: getOurCollection,
+  });
+
+  return (
+    <section>
+      <h2>Our Collection</h2>
+    </section>
+  );
+}
