8ab1a8de0216995e782ad1c34d7f7bfbe90fb5cf
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-01 16:01:49 -0600

    Home QueryClientProvider test

--- src/__TEST__/components/home/home.test.tsx
+++ src/__TEST__/components/home/home.test.tsx
@@ -2,12 +2,16 @@ import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import Home from "../../../components/home/Home";
 import { MemoryRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../../react-query";
 
 describe("Home", () => {
   it("should render correctly", () => {
     render(
       <MemoryRouter>
-        <Home />
+        <QueryClientProvider client={queryClient}>
+          <Home />
+        </QueryClientProvider>
       </MemoryRouter>
     );
 
