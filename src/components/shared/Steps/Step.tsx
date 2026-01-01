203b4460b4b40cbea4673fd090609c1c750d953a
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-01-01 17:04:35 -0600

    Steps TSX

--- src/components/shared/Steps/Step.tsx
+++ src/components/shared/Steps/Step.tsx
@@ -2,10 +2,10 @@ import type { StepProps } from "./types";
 
 export default function Step(props: StepProps) {
   return (
-    <div>
+    <li>
       <span>0{props.num}</span>
       <h3>{props.title}</h3>
       <p>{props.desc}</p>
-    </div>
+    </li>
   );
 }
---
+++ src/components/shared/Steps/Steps.tsx
@@ -0,0 +1,30 @@
+import Step from "./Step";
+
+export default function Steps() {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <ul>
+        <Step
+          num={1}
+          title="Pick your coffee"
+          desc="Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out."
+        />
+        <Step
+          num={2}
+          title="Choose the frequency"
+          desc="Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal."
+        />
+        <Step
+          num={3}
+          title="Receive and enjoy!"
+          desc="We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience."
+        />
+      </ul>
+    </div>
+  );
+}
