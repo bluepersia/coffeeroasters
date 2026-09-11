47bc67b420d053d96114316e712403d0de1dbded
Marco Enrique <fuzzyslash@hotmail.com>
2026-09-11 12:41:00 -0600

    Setup mixins & utilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import type { JSX } from "react/jsx-runtime";
-import "./App.css";
+import "./App.scss";
 
 function App(): JSX.Element {
   return (
-    <>
-      <h1>Hello World</h1>
-      <p>Some lorem ipsum text</p>
-    </>
+    <div className="container">
+      <h1 className="text1">The quick brown fox jumps over the lazy dog.</h1>
+      <p className="text2">The quick brown fox jumps over the lazy dog. 2</p>
+      <p className="text3">The quick brown fox jumps over the lazy dog. 3</p>
+      <p className="text4">The quick brown fox jumps over the lazy dog. 4</p>
+      <p className="text5">The quick brown fox jumps over the lazy dog. 5 </p>
+      <p className="text6">The quick brown fox jumps over the lazy dog. 6</p>
+      <p className="text7">The quick brown fox jumps over the lazy dog. 7</p>
+      <p className="text8">The quick brown fox jumps over the lazy dog. 8</p>
+    </div>
   );
 }
 
