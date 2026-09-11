06e8caacd2d8e030e31649f6454c7930f7b9b1ba
Marco Enrique <fuzzyslash@hotmail.com>
2026-09-11 12:45:15 -0600

    Setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import type { JSX } from "react/jsx-runtime";
 import "./App.scss";
+import { BrowserRouter, Routes } from "react-router";
 
 function App(): JSX.Element {
   return (
-    <div className="container">
-      <h1 className="text1">The quick brown fox jumps over the lazy dog.</h1>
-      <p className="text2">The quick brown fox jumps over the lazy dog. 2</p>
-      <p className="text3">The quick brown fox jumps over the lazy dog. 3</p>
-      <p className="text4">The quick brown fox jumps over the lazy dog. 4</p>
-      <p className="text5">The quick brown fox jumps over the lazy dog. 5 </p>
-      <p className="text6">The quick brown fox jumps over the lazy dog. 6</p>
-      <p className="text7">The quick brown fox jumps over the lazy dog. 7</p>
-      <p className="text8">The quick brown fox jumps over the lazy dog. 8</p>
-    </div>
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
   );
 }
 
